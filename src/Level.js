6c91b268a4ba05a8434cf0c46af8dc0034cfbc50
David Oberhofer <oberhofer.david@gmail.com>
2016-04-20 09:38:28 +0200

    added level type for future use

--- src/Level.js
+++ src/Level.js
@@ -1,6 +1,7 @@
 var chalk = require('chalk');
 
-var Level = function(str, int, printStackTraces, colorModifier) {
+var Level = function(level, str, int, printStackTraces, colorModifier) {
+    this.level = level;
     this.str = str;
     this.int = int;
     this.printStackTraces = printStackTraces;
@@ -10,10 +11,10 @@ Level.prototype.constructor = Level;
 
 module.exports = {
     Level: Level,
-    ALL:   new Level("ALL  ", 500,  false, null),
-    DEBUG: new Level("DEBUG", 1000, false, null),
-    INFO:  new Level("INFO ", 1500, false, null),
-    WARN:  new Level("WARN ", 2000, false, chalk.yellow),
-    ERROR: new Level("ERROR", 4000, true , chalk.red),
-    FATAL: new Level("FATAL", 5000, true , chalk.red)
+    ALL:   new Level("all",   "ALL  ", 500,  false, null),
+    DEBUG: new Level("debug", "DEBUG", 1000, false, null),
+    INFO:  new Level("info",  "INFO ", 1500, false, null),
+    WARN:  new Level("warn",  "WARN ", 2000, false, chalk.yellow),
+    ERROR: new Level("error", "ERROR", 4000, true , chalk.red),
+    FATAL: new Level("fatal", "FATAL", 5000, true , chalk.red)
 };
